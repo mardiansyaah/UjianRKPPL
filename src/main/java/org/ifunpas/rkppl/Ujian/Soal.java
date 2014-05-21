3e51006677d286056feabb94fe8898faf48ce438
604-10 <604-10@604-10.labif.local>
2014-05-21 15:43:40 +0700

    GIT netbean

--- src/main/java/org/ifunpas/rkppl/Ujian/Soal.java
+++ src/main/java/org/ifunpas/rkppl/Ujian/Soal.java
@@ -9,5 +9,24 @@ package org.ifunpas.rkppl.Ujian;
  * @author 604-10
  */
 public class Soal {
-    
+    public int pangkat(int bil, int pangkat){
+        int hasil = 0;
+        int n = 1;
+        while (n <= pangkat) {            
+            n++;
+            hasil *=bil;
+            
+        }
+        return hasil;
+    }
+    public int faktorial (int bil){
+        int hasil =1;
+        for (int i = 0; i < bil; i++) {
+            hasil *= i;
+        }
+        return hasil;
+    }
+    public int perkalian (int bilA, int bilB){
+        return bilA * bilB;
+    }
 }
