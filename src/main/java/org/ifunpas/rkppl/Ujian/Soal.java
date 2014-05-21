08dabf8065185f4ba515b228e7ef0891adba85f4
604-10 <604-10@604-10.labif.local>
2014-05-21 15:34:55 +0700

    test GIT netbean

---
+++ src/main/java/org/ifunpas/rkppl/Ujian/Soal.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ifunpas.rkppl.Ujian;
+
+/**
+ *
+ * @author 604-10
+ */
+public class Soal {
+    
+}
